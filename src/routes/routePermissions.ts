81c7f2260a47b509d3f442a208777f31eea4b72d
Devon Norris <devon.norris@avoxi.com>
2020-04-15 22:03:55 -0400

    update route perms

--- src/routes/routePermissions.ts
+++ src/routes/routePermissions.ts
@@ -1,12 +1,14 @@
 import routes from './routes'
 import permissions from './permissions'
-const { home, login, logout, signup, manageUsers } = routes
-const { PUBLIC, users_u } = permissions
+const { home, login, logout, signup, userSettings, manageUsers, manageOrganizations } = routes
+const { PUBLIC, users_u, users_d, organizations_d } = permissions
 
 export default {
   [home]: [PUBLIC],
   [login]: [PUBLIC],
   [logout]: [PUBLIC],
   [signup]: [PUBLIC],
-  [manageUsers]: [users_u],
+  [userSettings]: [users_u],
+  [manageUsers]: [users_d],
+  [manageOrganizations]: [organizations_d],
 }
